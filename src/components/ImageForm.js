34f70946f6014554e64ed91acadcffd05b3dcf4e
mayankshah1607 <mayankm.shah2017@vitstudent.ac.in>
2018-08-21 15:33:15 +0530

    Changed styles

--- src/components/ImageForm.js
+++ src/components/ImageForm.js
@@ -6,10 +6,11 @@ const ImageForm = ({onTextChange, onButtonSubmit}) => {
     return(
         <div>
             
-            <p className='fw6' style={{color:'white'}}>{'Choose an image to perform demographic classification.'}</p>
+            <p className='fw6 f3 centre' style={{color:'white'}}>{'Demographical predictions using Machine Learning.'}</p><br/>
+            <p className='fw6 f5 centre' style={{color:'white'}}>{'Continue by entering the URL of the person\'s image'}</p><br/>
                 <div className='centre mh4 ba b--white bw3 br4 bg-white'>
                     <input className='f5 pa3 ba bw0 br4' type='text' onChange={onTextChange} placeholder='Enter image URL...'/>
-                    <button onClick={onButtonSubmit} className='f6 fw6 grow br4 ph4 dib white bg-light-red ba bw0'>Predict</button>
+                    <button onClick={onButtonSubmit} className='f6 fw6 grow br4 ph4 dib white bg-blue ba bw0'>Predict</button>
                 </div>
             </div>
     );
