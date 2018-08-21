2cc16a986228e3394bbe06601ead9f3ac5e2fe3a
mayankshah1607 <mayankm.shah2017@vitstudent.ac.in>
2018-08-21 19:58:40 +0530

    Minor fixes

--- src/components/ImageForm.js
+++ src/components/ImageForm.js
@@ -8,7 +8,7 @@ const ImageForm = ({onTextChange, onButtonSubmit}) => {
             
             <p className='fw6 f3 centre mv3' style={{color:'white'}}>{'Demographical predictions using Machine Learning.'}</p><br/>
             <p className='fw6 f5 centre mv3' style={{color:'white'}}>{'Continue by entering the URL of the person\'s image'}</p><br/>
-                <div className='centre ba pa0 b--white bw3 br4 bg-white'>
+                <div className='centre flex ba pa0 b--white bw3 br4 bg-white'>
                     <input className='f5 pa3 ba bw0 br4' type='text' onChange={onTextChange} placeholder='Enter image URL...'/>
                     <button onClick={onButtonSubmit} className='f6 fw6 grow br3 ph4 dib white bg-blue ba bw0'>Predict</button>
                 </div>
