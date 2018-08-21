8ac8f6ecaf89bd02a458453f9b1311120bfb690d
mayankshah1607 <mayankm.shah2017@vitstudent.ac.in>
2018-08-21 12:37:08 +0530

    Published

--- src/components/ImageForm.js
+++ src/components/ImageForm.js
@@ -6,10 +6,10 @@ const ImageForm = ({onTextChange, onButtonSubmit}) => {
     return(
         <div>
             
-            <p className='fw3' style={{color:'white'}}>{'Choose an image to detect faces.'}</p>
+            <p className='fw6' style={{color:'white'}}>{'Choose an image to perform demographic classification.'}</p>
                 <div className='centre mh4 ba b--white bw3 br4 bg-white'>
                     <input className='f5 pa3 ba bw0 br4' type='text' onChange={onTextChange} placeholder='Enter image URL...'/>
-                    <button onClick={onButtonSubmit} className='f6 fw6 grow br4 ph4 dib white bg-light-red ba bw0'>Detect</button>
+                    <button onClick={onButtonSubmit} className='f6 fw6 grow br4 ph4 dib white bg-light-red ba bw0'>Predict</button>
                 </div>
             </div>
     );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,8 +4,9 @@ import 'tachyons';
 const Navigation = () => {
     return(
         <nav style={{display:'flex',justifyContent:'flex-end', }}>
-            <a style={{color: 'white'}} className="fw6 link dim pa3">Sign Out</a>
+            <a href='https://github.com/mayankshah1607/Face-Detection-Web-App' style={{color: 'white'}} className="fw6 link dim pa3">View repository</a>
         </nav>
+        
     );
 }
 
